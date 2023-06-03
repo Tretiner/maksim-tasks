b15fb0c7500da8424281b97a3364fa9a533239e6
Tretiner <alexandirina@inbox.ru>
2023-06-03 21:42:42 +0500

    Revert "Solved task 8"
    This reverts commit a590506b30b457d0d5a463140e3d238775e7180e.

--- Controllers/TextController.cs
+++ Controllers/TextController.cs
@@ -2,6 +2,7 @@
 using maxim_tasks.Models.Queries;
 using maxim_tasks.Utils.Sorters;
 using Microsoft.AspNetCore.Mvc;
+
 namespace maxim_tasks.Controllers;
 
 [Route("api/[controller]")]
@@ -11,29 +12,20 @@ public class TextController : ControllerBase
 	private readonly IConfiguration _config;
 	private readonly EvenOddTextReverser _evenOddTextReverser;
 
-	private readonly SemaphoreSlim _requestsLimitSemaphore;
-
 	public TextController(IConfiguration config, EvenOddTextReverser evenOddTextReverser)
 	{
 		_config = config;
 		_evenOddTextReverser = evenOddTextReverser;
-
-		_requestsLimitSemaphore = new SemaphoreSlim(_config.GetValue<int>("AppConfig:Settings:ParallelLimit"));
 	}
 
 	/// <response code="200">OK</response>
 	/// <response code="400">Text has non-english or non-lowered characters or blacklisted</response>
 	/// <response code="500">Unknown error</response>
-	/// <response code="503">Service is unavailable</response>
 	[HttpGet("even_odd_reverse")]
 	public async Task<ActionResult<EvenOddTextReverserResult>> EvenOddReverse([FromQuery] ReverseTextQuery query)
 	{
-		if (!_requestsLimitSemaphore.Wait(0))
-		{
-			return StatusCode(503, "Service is unavailable");
-		}
-
 		var blackList = _config.GetSection("AppConfig:Settings:BlackList").Get<List<string>>();
+
 		if (blackList.Contains(query.Text) == true)
 		{
 			return BadRequest($"{query.Text} is blacklisted");
@@ -59,10 +51,6 @@ public class TextController : ControllerBase
 		{
 			Console.WriteLine(ex.Message);
 			return StatusCode(500, "Unknown error");
-		}
-		finally
-		{
-			_requestsLimitSemaphore.Release();
 		}
 	}
 }
