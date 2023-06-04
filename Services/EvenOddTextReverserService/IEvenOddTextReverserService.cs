120cd9c271c2e98f8652cc83ea0c7cb08999b3d2
Tretiner <alexandirina@inbox.ru>
2023-06-04 15:55:17 +0500

    made EvenOddTextReverser a service

---
+++ Services/EvenOddTextReverserService/IEvenOddTextReverserService.cs
@@ -0,0 +1,9 @@
+﻿using maxim_tasks.Models;
+using maxim_tasks.Utils.Sorters;
+
+namespace maxim_tasks.Services.EvenOddTextReverserService;
+
+public interface IEvenOddTextReverserService
+{
+	public Task<EvenOddTextReverserResult> ReverseText(string? text, IStringSorter? sorter);
+}
